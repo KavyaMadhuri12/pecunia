d4486175c1d8ee94a1fdbdd00c0c6e233d14e9dc
ADMIN <ADMIN@EEE-NEWLAB-226>
2020-02-19 14:19:35 +0530

    succesful

---
+++ src/main/java/com/cg/pecunia/ui/Client.java
@@ -0,0 +1,16 @@
+package com.cg.pecunia.ui;
+
+import java.util.Scanner;
+
+import com.cg.pecunia.bean.Slip;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Scanner scr=new Scanner(System.in);
+		Slip slip=null;
+		
+
+	}
+
+}
